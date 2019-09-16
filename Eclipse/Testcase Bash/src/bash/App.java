223686bba4a2b472a3657b292c849ab8faf3f62c
Nageek15 <44506268+Nageek15@users.noreply.github.com>
2019-09-16 14:12:07 -0400

    Testcase bash started

---
+++ Eclipse/Testcase Bash/src/bash/App.java
@@ -0,0 +1,28 @@
+package bash;
+
+import gameutil.text.Console;
+
+public class App {
+	public static void main(String[] args) {
+		Console.s.setVisible(true);
+		Console.s.setTheme(Console.theme.shell2);
+		boolean running=true;
+		while (running) {
+			Console.s.print("->>");
+			String cmd=Console.s.readLine();
+			switch (cmd) {
+				case "q":
+					running=false;
+					System.exit(0);
+				break;
+				case "?":
+					Console.s.println("Commands: ");
+					Console.s.println("  q - quit");
+					Console.s.println("  t - new testcase*");
+					Console.s.println("  e - end testcase*");
+					Console.s.println("  ");
+				break;
+			}
+		}
+	}
+}
